07dcc3e2c0e3bbc78e72b940cd767de241110ea9
hfurkan <h.furkanoztrk@gmail.com>
2017-12-14 23:00:10 +0300

    Son değişiklikler

--- App.js
+++ App.js
@@ -1,36 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+
 
 import React, { Component } from 'react';
 import {
-  StyleSheet,
+  StyleSheet, View, TouchableOpacity, Text
 } from 'react-native';
-
-import { Router, Scene, Stack } from 'react-native-router-flux';
+import firebase from 'firebase';
 import Login from './src/pages/Login';
-import Signup from './src/pages/Signup';
+import Spinner from './src/components/spinner';
 
-const App = () => {
+export default class App extends Component<{}> {
+  state= { loggedIn: null };
+  componentWillMount() {
+    firebase.initializeApp(
+      {
+    apiKey: 'AIzaSyDhhNgb1cGC8MQNvgwen6X3IQPwZ6I7rH0',
+    authDomain: 'giris-758a8.firebaseapp.com',
+    databaseURL: 'https://giris-758a8.firebaseio.com',
+    projectId: 'giris-758a8',
+    storageBucket: 'giris-758a8.appspot.com',
+    messagingSenderId: '501599023554'
+  });
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      this.setState({ loggedIn: true });
+    } else {
+      this.setState({ loggedIn: false });
+    }
+  });
+}
+  clickLogout() {
+      firebase.auth().signOut();
+  }
+  renderContent() {
+  switch (this.state.loggedIn) {
+    case true:
+      return (
+        <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText} onPress={this.clickLogout.bind(this)}>Çıkış</Text>
+        </TouchableOpacity>
+      );
+      case false:
+        return (
+          <Login />
+        );
+    default:
+        return (
+          <View>
+          <Spinner size="large" />
+          </View>
+        );
+  }
+}
+render() {
   return (
-
-    <Router>
-      <Stack key="root">
-        <Scene key="login" component={Login} title="Login" hideNavBar={true}/>
-        <Scene key="register" component={Signup} title="Kayıt Menüsü"  />
-      </Stack>
-    </Router>
+    <View style={styles.container}>
+    {this.renderContent()}
+    </View>
   );
-};
-
+}
+}
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#455a64',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+     width: 300,
+     backgroundColor: '#1c313a',
+     borderRadius: 25,
+     marginVertical: 10,
+     paddingVertical: 11
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#ffffff',
+    textAlign: 'center'
   }
 });
-export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,25 +2,95 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Text
-
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert
 } from 'react-native';
-import { Actions } from 'react-native-router-flux'; // New code
+import firebase from 'firebase';
+import Spinner from '../components/spinner';
 import Logo from '../components/Logo';
-import Form from '../components/Form';
-
 
 export default class Login extends Component<{}> {
-  render() {
-    return (
+state={ email: '', password: '', loading: false };
+clickLogin() {
+  this.setState({ loading: true });
+  const { email, password } = this.state;
+
+  if (email === '' || password === '') {
+    this.setState({ loading: false });
+    Alert.alert(
+      'Mesaj',
+      'Her İki Alanda Dolu Olmalı!',
+      [
+        { text: 'Tamam', onPress: () => null }
+      ]
+    );
+  }
+  else {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(this.loginSucces.bind(this))
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(this.loginSucces.bind(this))
+      .catch(this.loginFail.bind(this));
+    });
+  }
+  }
+    loginSucces() {
+      console.log('başarılı');
+      this.setState({ loading: false });
+    }
+    loginFail() {
+      console.log('hatalı');
+        this.setState({ loading: false });
+      Alert.alert(
+        'Mesaj',
+        'Kullanıcı Adı veya Şifre hatalı!!',
+        [
+          { text: 'Tamam', onPress: () => null }
+        ]
+      );
+    }
+    renderButton() {
+      if (!this.state.loading) {
+        return (<TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText} onPress={this.clickLogin.bind(this)}>Giriş</Text>
+        </TouchableOpacity>);
+      }
+      return <Spinner size="small" />;
+    }
+
+
+render() {
+  return (
       <View style={styles.container}>
           <Logo />
-          <Form type="Login" />
+          <View style={styles.container}>
+        <TextInput
+        style={styles.inputBox}
+        placeholder="E-mail"
+        placeholderTextColor="#ffffff"
+        value={this.state.email}
+        onChangeText={email => this.setState({ email })}
+        />
+        <TextInput
+        secureTextEntry
+        style={styles.inputBox}
+        placeholder="Şifre"
+        placeholderTextColor="#ffffff"
+        value={this.state.password}
+        onChangeText={password => this.setState({ password })}
+        />
+        {this.renderButton()}
+
+          </View>
           <View style={styles.signupTextCont}>
           <Text style={styles.signupText}>
           Hesabınız yok mu?
           </Text>
-          <Text onPress={() => Actions.register()} style={styles.signupButton}>
+
+          <Text >
           Kayıt Ol
           </Text>
           </View>
@@ -31,11 +101,33 @@ export default class Login extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#455a64',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 3,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
   },
-  signupTextCont: {
+    inputBox: {
+      width: 300,
+      backgroundColor: 'rgba(255,255,255,0.3)',
+     borderRadius: 25,
+     paddingHorizontal: 16,
+     fontSize: 16,
+     color: '#ffffff',
+     marginVertical: 10
+   },
+   button: {
+      width: 300,
+      backgroundColor: '#1c313a',
+      borderRadius: 25,
+      marginVertical: 10,
+      paddingVertical: 11
+   },
+   buttonText: {
+     fontSize: 16,
+     fontWeight: '500',
+     color: '#ffffff',
+     textAlign: 'center'
+   },
+   signupTextCont: {
     flexGrow: 1,
     alignItems: 'flex-end',
     justifyContent: 'center',
@@ -45,10 +137,6 @@ const styles = StyleSheet.create({
   signupText: {
     color: 'rgba(255,255,255,0.7)',
     fontSize: 16
-  },
-  signupButton: {
-    color: '#ffffff',
-    fontSize: 16,
-    fontWeight: '500'
   }
+
   });
